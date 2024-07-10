3d15edbe6fb1e539385f734d0e4e625391a3fb2d
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-07-10 16:13:51 +0200

    Update console.windows-details.for-windows.cpp

--- microlibs/cppm/basics/environment/console.windows-details.for-windows.cpp
+++ microlibs/cppm/basics/environment/console.windows-details.for-windows.cpp
@@ -23,20 +23,17 @@ auto cppm::impl::is_a_windows_console( FILE* f )
 auto cppm::impl::get_windows_console_kind()
     -> Console_kind::Enum
 {
+    if( not GetConsoleWindow() ) {                      return Console_kind::none; }
     using Id = w::Console_host_id;
-
     const Id host_id = w::get_console_host();
     if( host_id.name == Id::windows_terminal ) {
         // Known WT version supporting UTF-8 input is 1.20, May 2024.
-       return (host_id.version >= winapi::Version( 1, 20 )
-            ? Console_kind::windows_terminal
-            : Console_kind::old_windows_terminal
-            );
-    } else if( host_id.name == Id::mintty ) {
-        return Console_kind::mintty;
-    } else if( host_id.name == Id::classic_console ) {
-        return Console_kind::windows_classic;
-    } else {
-        return Console_kind::unknown;
+                                                        return (host_id.version >= winapi::Version( 1, 20 )
+                                                            ? Console_kind::windows_terminal
+                                                            : Console_kind::old_windows_terminal
+                                                            );
+    } else if( host_id.name == Id::mintty ) {           return Console_kind::mintty;
+    } else if( host_id.name == Id::classic_console ) {  return Console_kind::windows_classic;
+    } else {                                            return Console_kind::unknown;
     }
 }
